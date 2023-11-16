91e1f59e03b88f25de073de1781364b67e4894f6
Ivan Grce <ivan.grce@microblink.com>
2023-11-16 11:03:22 +0100

    Updated KDocs and libs for v1.2.0

--- libs/installToLocalMaven.sh
+++ libs/installToLocalMaven.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=1.1.0
+version=1.2.0
 
 mvn install:install-file -Dfile="capture-core-$version.aar" -DpomFile=pom-capture-core.xml -DcreateChecksum=true
 mvn install:install-file -Dfile="capture-ux-$version.aar" -DpomFile=pom-capture-ux.xml -DcreateChecksum=true
