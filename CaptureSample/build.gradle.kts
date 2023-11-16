84c394b418550b4ddef1ec78feb151dfa7d9eace
Ivan Grce <ivan.grce@microblink.com>
2023-11-16 11:15:23 +0100

    Updated samples for v1.2.0

--- CaptureSample/build.gradle.kts
+++ CaptureSample/build.gradle.kts
@@ -1,14 +1,14 @@
 buildscript {
     val kotlin_compiler_extension_version by extra("1.4.7")
     val core_ktx_version by extra("1.12.0")
-    val material_version by extra("1.9.0")
+    val material_version by extra("1.10.0")
     val appcompat_version by extra("1.6.1")
     val compose_version by extra("1.4.3")
     val compose_bom_version by extra("2023.08.00")
-    val navigation_compose_version by extra("2.7.3")
+    val navigation_compose_version by extra("2.7.5")
     val camerax_version by extra("1.2.3")
     // Capture SDK version
-    val capture_version by extra("1.1.0")
+    val capture_version by extra("1.2.0")
 
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
