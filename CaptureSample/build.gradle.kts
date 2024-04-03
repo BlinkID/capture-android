59ecaf2cb1ec0bca7c6facd252582f545dadbccb
Ivan Grce <ivan.grce@microblink.com>
2024-04-03 11:52:40 +0200

    Updated sample apps for v1.3.0

--- CaptureSample/build.gradle.kts
+++ CaptureSample/build.gradle.kts
@@ -1,19 +1,19 @@
 buildscript {
     val kotlin_compiler_extension_version by extra("1.4.7")
     val core_ktx_version by extra("1.12.0")
-    val material_version by extra("1.10.0")
+    val material_version by extra("1.11.0")
     val appcompat_version by extra("1.6.1")
     val compose_version by extra("1.4.3")
     val compose_bom_version by extra("2023.08.00")
-    val navigation_compose_version by extra("2.7.5")
+    val navigation_compose_version by extra("2.7.7")
     val camerax_version by extra("1.2.3")
     // Capture SDK version
-    val capture_version by extra("1.2.0")
+    val capture_version by extra("1.3.0")
 
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("com.android.library") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
 }
