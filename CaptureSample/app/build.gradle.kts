48fd17e3ccb04fa90265aad97f7856109e05e5d9
Ivan Grce <ivan.grce@microblink.com>
2025-07-22 16:58:34 +0200

    Updated sample apps for v1.4.1

--- CaptureSample/app/build.gradle.kts
+++ CaptureSample/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.microblink.capture.sample"
-    compileSdk =  34
+    compileSdk =  35
 
     defaultConfig {
         applicationId = "com.microblink.capture.sample"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- CaptureSample/lib-common/build.gradle.kts
+++ CaptureSample/lib-common/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.microblink.capture.sample.common"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
